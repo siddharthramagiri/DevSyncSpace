9775a6eb4d38bca38d56221cc304c7a33fb12e21
Siddu Ramagiri <sidduramagiri3@gmail.com>
2025-06-17 21:52:03 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
-  webpack: (config : any, { isServer } : any) => {
+  serverExternalPackages: ['@prisma/client'], 
+
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
     if (isServer) {
-      config.plugins = [...config.plugins]
+      config.plugins = [...config.plugins];
     }
-    return config
+    return config;
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
